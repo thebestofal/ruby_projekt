5f0709bfe8e19ac216935efd6deab349147f0f39
urun <mturski@protonmail.com>
2020-01-02 19:40:15 +0100

    skeleton test_app_controller.rb

---
+++ spec/test_app_controller.rb
@@ -0,0 +1,50 @@
+require_relative("../app/src/app_controller")
+RSpec.describe AppController do
+    describe ".surface_folder_level" do
+        it("changes directory and removes .submodulized file") do
+            
+        end
+        it("throws error if file doesn't exist") do
+            
+        end
+    end
+
+    describe ".sub_folder_level" do
+        it("returns true if file .submodulized exists") do
+            
+        end
+        it("returns false if file .submodulized doesn't exists") do
+            
+        end
+    end
+
+    describe ".confirm_expected_subfolders_exist" do
+        it("creates .submodulized file") do
+            
+        end
+    end
+
+    describe ".master_has_subfolders_or_is_subfolder_already" do
+        it("creates .submodulized file") do
+            
+        end
+    end
+
+    describe ".folder_counter" do
+        it("creates .submodulized file") do
+            
+        end
+    end
+
+    describe ".initialize_submodule" do
+        it("creates .submodulized file") do
+            
+        end
+    end
+
+    describe ".automate" do
+        it("creates .submodulized file") do
+            
+        end
+    end
+end
