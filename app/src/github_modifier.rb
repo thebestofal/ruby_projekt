e6d7af1116ddb447ec89e1fef90ec2df48ff37d0
pzaleszczyk <50424024+pzaleszczyk@users.noreply.github.com>
2020-01-12 17:25:43 +0100

    Update github_modifier.rb

--- app/src/github_modifier.rb
+++ app/src/github_modifier.rb
@@ -35,13 +35,13 @@ class GithubModifier
   #github interaction
   def delete_online_repo(folder, account) # When we have the program running, we will update this.
     username = account[:user];password = account[:pass];
-    `curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder.split('/')[-1]}}`;#puts folder.split('/')[-1];
+    `curl -u --silent #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder.split('/')[-1]}}`;#puts folder.split('/')[-1];
   end
   def create_online_repo(folder, account)
-    `curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }' /dev/null`
+    `curl -u --silent "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }' /dev/null`
   end
   def check_online_repo(folder, account)
-    response = `curl -i https://api.github.com/repos/#{account[:user]}/#{folder}`
+    response = `curl -i --silent https://api.github.com/repos/#{account[:user]}/#{folder}`
     response = JSON.parse(response[response.index('{')..-1])
     response["message"].nil?
   end 
