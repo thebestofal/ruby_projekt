49dae3bf86b6f501f05a368e048ae78cfa0f77b7
Marek <marek.skrzypkowski98@gmail.com>
2020-01-12 16:23:21 +0100

    Create test_folder_setup.rb

---
+++ spec/test_folder_setup.rb
@@ -0,0 +1,79 @@
+require_relative("../app/src/folder_setup")
+require 'json'
+require 'io/console'
+require 'fileutils'
+
+
+RSpec.describe Folder_Setup do
+    # before do
+	# 	allow($stdout).to receive(:write)
+	# end
+    describe ".confirm_folder_exists" do
+        it("check if folder exists") do
+            folder_setup = Folder_Setup.new()
+
+        end
+    end
+
+    describe ".notify" do
+        it("when can't find directory")do
+        folder_setup = Folder_Setup.new()
+        allow($stdin).to receive(:gets).and_return("folder","test","test","test","test")
+        $stdin.gets
+        allow_any_instance_of(Inputs).to receive(:check).and_return(true)
+        object = Inputs.inputsToUser()
+        expect(folder_setup).to receive(:notify).with('dir', object)
+        folder_setup.notify('dir', object)
+        end
+    end
+
+    describe ".check_remote_exists" do
+        it("check when remote doesn't exists") do
+            folder_setup = Folder_Setup.new()
+            account = { :user => "user"}
+            expect(folder_setup.check_remote_exists(account, 'dir1')).to be_falsey
+        end
+
+        it("check when remote exists") do
+            folder_setup = Folder_Setup.new()
+            account = { :user => "user"}
+
+            #expect(folder_setup.check_remote_exists(account, 'dir1')).to be_truthy
+        end
+    end
+
+    describe ".check_local_directory_exists" do
+        it("check not existing local directory") do
+            folder_setup = Folder_Setup.new()
+            allow($stdin).to receive(:gets).and_return("folder","test","test","test","test")
+			$stdin.gets
+            allow_any_instance_of(Inputs).to receive(:check).and_return(true)
+            object = Inputs.inputsToUser()
+            expect(folder_setup.check_local_directory_exists('dir3', object)).to be_falsey
+        end
+
+        it("check existing local directory")do
+            folder_setup = Folder_Setup.new()
+            allow($stdin).to receive(:gets).and_return("folder","test","test","test","test")
+			$stdin.gets
+            allow_any_instance_of(Inputs).to receive(:check).and_return(true)
+            object = Inputs.inputsToUser()
+            str = "../dir1/"
+            str.concat(object[:f])
+            FileUtils.mkdir_p str
+            expect(folder_setup.check_local_directory_exists('dir1', object)).to be_truthy
+        end
+    end
+
+    describe ".clone_master" do
+        it("remove folders and clone master")do
+            folder_setup = Folder_Setup.new()
+            allow($stdin).to receive(:gets).and_return("folder","test","test","test","test")
+            $stdin.gets
+            allow_any_instance_of(Inputs).to receive(:check).and_return(true)
+            object = Inputs.inputsToUser()
+            expect(folder_setup).to receive(:clone_master).with('dir', object)
+            folder_setup.clone_master('dir', object)
+        end
+    end
+end
